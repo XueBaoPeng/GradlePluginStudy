49ff790d519c01e6090f1dba5efbf4a97dc0d354
xuebp <13519297683@163.com>
2017-12-01 10:20:04 +0800

    Initial commit

---
+++ plugingradle/src/main/groovy/MyPlugin.groovy
@@ -0,0 +1,15 @@
+package com.xbp.myplugingradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+public class MyPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        System.out.println("========================");
+        System.out.println("hello gradle plugin!");
+        System.out.println("========================");
+    }
+}
